6be27ca30792096a7a070bebfcf93bdd4a56c15b
Liam Butcher <liam.butcher@hotmail.com>
2017-11-20 17:26:08 -0800

    Added sushi/cafe searching to reload. Fixed drag/drop bug

--- js/SearchResults.js
+++ js/SearchResults.js
@@ -3,9 +3,17 @@
  */
 
 
+var option1Taken = false
+var option2Taken = false
+var option1 =null
+var option2 =null
+var sushi1Html = "<img  id=lsushi2' src='./img/sushi2.png'>"
+
+
 
 //From w3 Schools
 function allowDrop(ev) {
+    //console.log(ev)
     ev.preventDefault();
 }
 
@@ -13,10 +21,43 @@ function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
-function drop(ev) {
-    document.getElementById("div1").innerHTML = "";
+function drop(ev,loc) {
+    if(!option1Taken&&loc=='top') {
+        document.getElementById("div1").innerHTML = "";
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("text");
+        ev.target.appendChild(document.getElementById(data));
+        option1=document.getElementById("div1").innerHTML
+        console.log(data)
+        option1Taken=true
+    }
+    else if(!option2Taken&&loc=='bottom') {
+        document.getElementById("div2").innerHTML = "";
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("text");
+        ev.target.appendChild(document.getElementById(data));
+        option1=document.getElementById("div2").innerHTML
+        console.log(data)
+        option2Taken=true
+    }
+}
 
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+function updateCompare(){
+    console.log(option1Taken)
+    if(option1!=null){
+        document.getElementById("div1").innerHTML = option1;
+    }
+
+}
+
+function loadResults(type){
+    if(type=='sushi'){
+        document.getElementById("resultsLocCafe").style.display='none'
+        document.getElementById("resultsLocSushi").style.display='block'
+    }
+    else{
+        document.getElementById("resultsLocSushi").style.display='none'
+        document.getElementById("resultsLocCafe").style.display='block'
+
+    }
 }
